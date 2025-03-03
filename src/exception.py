4fc44674c0796706978f17cd41543e96817cf2d3
vishalpr013 <vishalprajapati9427@gmail.com>
2025-03-03 17:07:09 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -21,10 +21,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
-    
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Logging has started")
-        raise CustomException(e,sys)
